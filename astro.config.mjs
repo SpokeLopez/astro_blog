fef66e859f5ddc38bd31a55c18bc7e86573695b5
Eduardo López <spoke.asafmexico@hotmail.com>
2026-04-19 16:14:55 -0600

    Se agrega comando para deploy y configuración de seenode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://spokelopez.com',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
